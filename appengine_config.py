05c4a19ec560ee09b273b02253effe6f7bb520d9
Jason Schray <jschray@calpoly.edu>
2017-04-27 22:52:40 -0700

    completed basic deckbook view

--- appengine_config.py
+++ appengine_config.py
@@ -1,5 +1,15 @@
 # appengine_config.py
 from google.appengine.ext import vendor
-
+#import vendor
 # Add any libraries install in the "lib" folder.
 vendor.add('lib')
+
+# Patch os.path.expanduser. This should be fixed in GAE
+# versions released after Nov 2016.
+import os.path
+
+
+def patched_expanduser(path):
+    return path
+
+os.path.expanduser = patched_expanduser
