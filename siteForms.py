b3212def6177f0d8739c638b74abc11957e409de
Jason Schray <jschray@calpoly.edu>
2017-05-02 07:30:50 -0700

    basic user accounts being made

--- siteForms.py
+++ siteForms.py
@@ -6,3 +6,10 @@ class loginForm(Form):
     submit       = SubmitField ('Submit')
 
 
+class signupForm(Form):
+    username     = StringField('Username', [validators.Length(min=4, max=25)])
+    password     = PasswordField('Password')
+    passwordRepeat     = PasswordField('Repeat Password')
+    email = StringField('Email', [validators.Length(min=4, max=100)])
+    submit       = SubmitField ('Submit')
+
